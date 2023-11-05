609a62b415f2953d8c34cbc3c355f33a0155e79b
Mojtaba Rahbari <mojtaba_netco@yahoo.com>
2023-11-05 11:22:31 +0100

    Update factory-utilize.php

--- factory/factory-utilize.php
+++ factory/factory-utilize.php
@@ -6,15 +6,15 @@
  * In factory pattern
  * 1- An interface is required to define the methods that each class should implement. (PaymentInterface.php)
  * 2- A factory class is needed to invoke and manage the implemented classes. (PaymentFactory.php)
- * 3- Business-related classes are needed to handle specific tasks, and they should implement the defined interfaces. (Paypal.php, CreditCard.php)
+ * 3- Business-related classes are needed to handle specific tasks, and they should implement the defined interfaces. (Paypal.php, Klarna.php)
  * 4- To utilize the factory class, a file such as a controller should be created with specific parameters. (factory-utilize.php)
  */
 include "./PaymentFactory.php";
 
 // Usage example
 try {
-    $creditCard = PaymentFactory::create('credit-card', ['cardNumber' => '1234 5678 9012 3456', 'expirationDate' => '01/23', 'cvv' => '123']);
-    $creditCard->charge(12555100);
+    $klarna = PaymentFactory::create('credit-card', ['cardNumber' => '1234 5678 9012 3456', 'expirationDate' => '01/23', 'cvv' => '123']);
+    $klarna->charge(12555100);
 } catch (Exception $e) {
 }
 
