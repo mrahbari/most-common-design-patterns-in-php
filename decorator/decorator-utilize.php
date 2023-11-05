cd6d58f1a64a708bd2b3bd120c7643a12ffd752e
Mojtaba Rahbari <mojtaba_netco@yahoo.com>
2023-11-05 11:18:28 +0100

    Update decorator-utilize.php

--- decorator/decorator-utilize.php
+++ decorator/decorator-utilize.php
@@ -1,5 +1,6 @@
 <?php
 /**
+ * Decorator design pattern is used to dynamically add additional tasks to the current logic/bussiness.
  * In this example, we have a Payment class that implements the PaymentInterface, just like in the previous example.
  * We then have two decorators: KlarnaDecorator and PayPalDecorator.
  *
@@ -8,17 +9,16 @@
 
  * The PayPalDecorator takes in the email and password for the PayPal account as constructor parameters. In the pay method, it first logs in to the PayPal account by calling the loginToPayPal method, and then calls the pay method of the decorated payment object. Finally, it echoes a message to indicate that the payment was processed with the PayPal account.
 
- * In the example usage section, we create a Payment object and decorate it first with the KlarnaDecorator and then with the PayPalDecorator. We then call the `
  *
  *
  * In this example, we first create a new Payment object. We then decorate it with a KlarnaDecorator and a PayPalDecorator to allow payments to be processed using a credit card and a PayPal account. We also decorate it with a PaymentLoggerDecorator to log all payment transactions and a PaymentRetryDecorator to automatically retry failed payments up to three times.
  * Finally, we call the pay() method on the decorated Payment object to process a payment of $100. If an exception is thrown during payment processing, we catch it and display an error message.
  *
  * In decorator pattern
- * 1- An interface is required to define the methods that each class should implement. (PaymentMethod.php)
- * 2- A factory class is needed to invoke and manage the implemented classes. (PaymentMethodFactory.php)
- * 3- Business-related classes are needed to handle specific tasks, and they should implement the defined interfaces. (Paypal.php, Klarna.php)
- * 4- To utilize the factory class, a file such as a controller should be created with specific parameters. (factory-utilize.php)
+ * 1- An interface is required to define the methods that each class should implement. (PaymentInterface.php)
+ * 2- An abstract class is needed to invoke and manage the implemented classes. (PaymentDecorator.php)
+ * 3- Business-related classes are needed to handle specific tasks, and they should implement the defined interfaces. (PaypalDecorator.php, KlarnaDecorator.php, etc.)
+ * 4- To utilize the Decorator classes, a file such as a controller should be created with specific parameters. (decrator-utilize.php)
  */
 // Require the classes
 require_once 'PaymentInterface.php';
